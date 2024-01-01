0b38aff5883bd2f256c632fdece46a7ccfb8e391
hiroto-hara <xre0605@gmail.com>
2024-01-01 22:35:42 +0900

    emitの基礎

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,21 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import Input from './components/Input.vue';
-import Radio from './components/Radio.vue';
-import Select from './components/Select.vue';
+import OneSection from './components/OneSection.vue';
 
-const currentComp = ref(Input);
-const currentCompName = ref("Input");
-const compList = [Input, Radio, Select];
-const compNameList = ["Input", "Radio", "Select"];
+const randInit = Math.round(Math.random() * 10);
+const rand = ref(randInit);
 
-let currentCompIndex = 0;
-const switchComp = (): void => {
-  currentCompIndex++;
-  if (currentCompIndex >= 3) {
-    currentCompIndex = 0;
-  }
-  currentComp.value = compList[currentCompIndex];
-  currentCompName.value = compNameList[currentCompIndex];
+const onCreateNewRand = (): void => {
+  rand.value = Math.round(Math.random() * 10);
 }
 </script>
 
 <template>
-  <p>コンポーネント名： {{ currentCompName }}</p>
-  <KeepAlive>
-    <component v-bind:is="currentComp" />
-  </KeepAlive>
-  <button v-on:click="switchComp">切り替え</button>
+  <section>
+    <p>親コンポーネントで乱数を表示: {{ rand }}</p>
+    <OneSection
+    v-bind:rand="rand"
+    v-on:createNewRand="onCreateNewRand"
+    />
+  </section>
 </template>
