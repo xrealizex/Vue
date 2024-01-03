698409334357f71558af7eab12c1bb5a670adca6
hiroto-hara <xre0605@gmail.com>
2024-01-03 17:53:09 +0900

    emitでのデータ送信

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,44 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import OneSection from './components/OneSection.vue';
+import { computed, ref } from 'vue';
+import type { Member } from './interfaces';
+import OneMember from './components/OneMember.vue';
 
-const randInit = Math.round(Math.random() * 10);
-const rand = ref(randInit);
+const memberListInit = new Map<number, Member>();
+memberListInit.set(33456, {id: 33456, name: "田中太郎", email: "bow@example.com", points: 35, note: "初回入会特典あり。"});
+memberListInit.set(47783, {id: 47783, name: "鈴木二郎", email: "mue@example.com", points: 53});
+const memberList = ref(memberListInit);
 
-const onCreateNewRand = (): void => {
-  rand.value = Math.round(Math.random() * 10);
+const totalPoints = computed(
+  (): number => {
+    let total = 0;
+    for(const member of memberList.value.values()) {
+      total += member.points;
+    }
+    return total
+  }
+)
+
+const onIncrementPoint = (id: number): void => {
+  const member = memberList.value.get(id);
+  if(member != undefined) {
+    member.points++;
+  }
 }
 </script>
 
 <template>
   <section>
-    <p>親コンポーネントで乱数を表示: {{ rand }}</p>
-    <OneSection
-    v-bind:rand="rand"
-    v-on:createNewRand="onCreateNewRand"
+    <h1>会員リスト</h1>
+    <p>全会員の保有ポイントの合計：{{ totalPoints }}</p>
+    <OneMember
+      v-for="[id, member] in memberList"
+      v-bind:key="id"
+      v-bind:id="id"
+      v-bind:name="member.name"
+      v-bind:email="member.email"
+      v-bind:points="member.points"
+      v-bind:note="member.note"
+      v-on:incrementPoint="onIncrementPoint"
     />
   </section>
 </template>
