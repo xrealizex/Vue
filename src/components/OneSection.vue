c1959a674a626303dca3004f55fa9bd195017349
hiroto-hara <xre0605@gmail.com>
2024-01-05 22:09:04 +0900

    slotの利用

--- src/components/OneSection.vue
+++ src/components/OneSection.vue
@@ -1,23 +1,14 @@
 <script setup lang="ts">
 interface Props {
-  rand: number;
+  name: string;
 }
-interface Emits {
-  (event: "createNewRand"): void; 
-}
-
 defineProps<Props>();
-const emit = defineEmits<Emits>();
-
-const onNewRandButtonClick = (): void => {
-  emit("createNewRand");
-}
 </script>
 
 <template>
   <section class="box">
-    <p>子コンポーネントの乱数を表示: {{ rand }}</p>
-    <button v-on:click="onNewRandButtonClick">新たな乱数を発生</button>
+    <h1>{{ name }}さんの状況</h1>
+    <slot/>
   </section>
 </template>
 
